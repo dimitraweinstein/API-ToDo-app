9c4310a85ba06f26b7679f74725ce02dafb7dd0a
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-06-29 16:08:57 -0700

    get/ todos endpoint works and shows seed data

--- data/todos.js
+++ data/todos.js
@@ -1,22 +1,38 @@
 module.exports = [
   {
+    id: 1,
+    todo_item: 'wash the dishes',
+    is_completed: false,
+    user_id: 1
+  },
+  {
+    id: 2,
     todo_item: 'clean bathroom',
     is_completed: false,
+    user_id: 1
   },
   {
+    id: 3,
     todo_item: 'wash dog',
     is_completed: false,
+    user_id: 1
   },
   {
+    id: 4,
     todo_item: 'mop & sweep floors',
     is_completed: false,
+    user_id: 1
   },
   {
+    id: 5,
     todo_item: 'pick up groceries',
     is_completed: false,
+    user_id: 1
   },
   {
+    id: 6,
     todo_item: 'pull weeds, water plants',
     is_completed: false,
+    user_id: 1
   }
 ];
