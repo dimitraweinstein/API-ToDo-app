760cd82f289e763b40f9b7355961802e8c2733cd
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-06-29 19:21:43 -0700

    3 tests passing: get/ , post/ , and put/ todos routes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -112,5 +112,43 @@ describe('app routes', () => {
 
       expect(data.body).toEqual(expectation);
     });
+
+    test('update todos', async() => {
+
+      const expectation = [
+        {
+          'id': 7,
+          'todo_item': 'brush teeth',
+          'is_completed': false,
+          'owner_id': 2
+        },
+        {
+          'id': 8,
+          'todo_item': 'comb hair',
+          'is_completed': false,
+          'owner_id': 2
+        },
+        {
+          'id': 9,
+          'todo_item': 'brush teeth',
+          'is_completed': false,
+          'owner_id': 2
+        },
+        {
+          'id': 10,
+          'todo_item': 'jump rope',
+          'is_completed': false,
+          'owner_id': 2
+        }
+      ];
+
+      const data = await fakeRequest(app)
+        .get('/api/todos/10')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
   });
 });
